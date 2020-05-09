6826083cc1c00c31e26c13a0215ffc8b55a66e61
Fernando Peixoto <fps.engenheiro@gmail.com>
2020-05-09 11:14:54 -0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname + '/dist'));
+const nomeApp = process.env.npm_package_name;
+const app = express();
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+ 
 app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + 'dist/index.html'));
-})
 
-console.log('Ouvindo console');
+
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// app.use(express.static(__dirname + '/dist'));
+// app.listen(process.env.PORT || 8080);
+// app.get('/*', function(req, res){
+//     res.sendFile(path.join(__dirname + 'dist/index.html'));
+// })
+
+// console.log('Ouvindo console');
